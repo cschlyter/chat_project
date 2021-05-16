050796cb60f03cb7c564c142eec55594d741dca5
Carlos Schlyter <carlos.schlyter@gmail.com>
2021-05-16 15:43:41 -0300

    User management

--- chat/urls.py
+++ chat/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.register, name='register'),
-    path('login', views.login, name='login'),
+    path('users/register/', views.register, name='register'),
+    path('users/login/', views.login_page, name='login'),
+    path('users/logout/', views.logout_page, name='logout'),
 
     path('', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
--- chat/views.py
+++ chat/views.py
@@ -1,20 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='login')
 def index(request):
     return render(request, 'chat/index.html')
 
 
 def register(request):
-    context = {}
-    return render(request, 'chat/register.html', context)
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        form = UserCreationForm()
+
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('login_page')
+
+        context = {'form': form}
+        return render(request, 'chat/register.html', context)
 
 
-def login(request):
-    context = {}
-    return render(request, 'chat/login.html', context)
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+
+        context = {}
+        return render(request, 'chat/login.html', context)
 
 
+@login_required(login_url='login')
+def logout_page(request):
+    logout(request)
+    return redirect('login_page')
+
+
+@login_required(login_url='login')
 def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
