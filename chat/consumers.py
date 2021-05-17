d999243bf70a2cc68100975d7f2ae55f14db2305
Carlos Schlyter <carlos.schlyter@gmail.com>
2021-05-16 22:21:42 -0300

    Chat bot messages being sent to chat

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,9 +1,9 @@
 import json
 import requests
-import csv
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 from channels.consumer import SyncConsumer
+from channels.layers import get_channel_layer
 
 from chat.models import Room, Message
 
@@ -34,25 +34,30 @@ class ChatConsumer(WebsocketConsumer):
         message = text_data_json['message']
         user = text_data_json['user']
 
-        if message[0] == "/":
+        if message != "" and message[0] == "/":
             stock_name = "aapl.us"
-            async_to_sync(self.channel_layer.send)('background-tasks', {'type': 'stock', 'name': stock_name})
+            async_to_sync(self.channel_layer.send)('background-tasks', {
+                'type': 'stock',
+                'name': stock_name,
+                'room_name': self.room_name,
+                'room_group_name': self.room_group_name
+            })
+        else:
+            room = Room.objects.get(name=self.room_name)
 
-        room = Room.objects.get(name=self.room_name)
+            m = Message(room=room, message=message, user=user)
+            m.save()
 
-        m = Message(room=room, message=message, user=user)
-        m.save()
-
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message,
-                'user': user,
-                'date': m.formatted_timestamp
-            }
-        )
+            # Send message to room group
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': message,
+                    'user': user,
+                    'date': m.formatted_timestamp
+                }
+            )
 
     # Receive message from room group
     def chat_message(self, event):
@@ -72,12 +77,30 @@ class BackgroundTaskConsumer(SyncConsumer):
 
     def stock(self, message):
         stock_name = message.get("name")
-        url = f"https://stooq.com/q/l/?s={stock_name}&f=sd2t2ohlcv&h&e=csv"
+        room_name = message.get("room_name")
+        room_group_name = message.get("room_group_name")
+        url = f"https://stooq.com/q/l/?s={stock_name.lower()}&f=sd2t2ohlcv&h&e=csv"
 
         r = requests.get(url)
         csv_file = r.content
-
         data = csv_file.decode('utf-8').splitlines()
         share_value = data[1].split(",")[6]
-        message = f"{stock_name} quote is {share_value} per share"
-        print(message)
+        robot_message = f"{stock_name.upper()} quote is {share_value} per share"
+
+        channel_layer = get_channel_layer()
+        user = "Stock Robot"
+
+        room = Room.objects.get(name=room_name)
+        m = Message(room=room, message=robot_message, user=user)
+        m.save()
+
+        async_to_sync(channel_layer.group_send)(
+            room_group_name,
+            {
+                'type': 'chat_message',
+                'message': robot_message,
+                'user': user,
+                'date': m.formatted_timestamp
+            }
+        )
+
